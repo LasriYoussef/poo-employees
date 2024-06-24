3d10711fecb2d530974f2a3a6c78a17227d131e5
Amazir Lasri <lamazir@gmail.com>
2024-06-24 15:28:08 +0200

    Correction setregistarionNumber

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -1,5 +1,12 @@
 package fr.afpa.employees;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
 /*
 Objectif :
 L’objectif de ce TP est de concevoir un programme en console basé sur une approche objet et
@@ -9,31 +16,157 @@ Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
 
-class Employee
-{
+public class Employee {
 	/**
 	 * Matricule de l'employé
 	 */
+	// compléter les attributs comme présenté dans le PDF
 	private String registrationNumber;
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private final int socialRate = 30;
+	private LocalDate birthday;
 
-	// TODO compléter les attributs comme présenté dans le PDF
-
-	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	// compléter le constructeur de la classes
+	public Employee(String registrationNumber, String lastName, String firstName, double salary,
+			String birthday) {
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
+		this.birthday = LocalDate.parse(birthday);
+
 	}
 
-	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+	// ajouter les getters et les setters
+
+	// Getters
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
 
-	public void setRegistrationNumber(String registrationNumber) throws Exception {
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public int getSocialRate() {
+		return socialRate;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	// Setters
+	public void setRegistrationNumber(String registrationNumber) {
 		this.registrationNumber = registrationNumber;
 	}
 
-	// TODO ajouter les getters et les setters
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
-	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+
+		// Implémenter les setters et getters de la classe (permet d'accéder aux
+		// attributs privés)
+
+	}
+
+	// Implémenter la méthode "toString()" qui renvoie une chaîne de caractère
+	@Override
+	public String toString() {
+		return "Employee [\n registrationNumber=" + registrationNumber
+				+ "\n lastName=" + lastName
+				+ "\n firstName=" + firstName
+				+ "\n salary=" + salary
+				+ "\n socialRate=" + socialRate
+				+ "\n birthday=" + birthday + "]";
+	}
+
+	public String fullName() {
+		return this.lastName + this.firstName;
+	}
+
+	public double netSalary() {
+		double netSalary = this.salary - this.salary * (this.socialRate / 100);
+		return netSalary;
+	}
+
+	public long daysBeforeBirthday() {
+		LocalDate currentDate = LocalDate.now();
+		LocalDate nextBirthday = this.birthday.withYear(currentDate.getYear());
+
+		if (nextBirthday.isBefore(currentDate) || nextBirthday.isEqual(currentDate)) {
+			nextBirthday = nextBirthday.plusYears(1);
+		}
+		return ChronoUnit.DAYS.between(currentDate, currentDate);
+	}
+
+/**
+     * Vérifie une chaîne de caractères et indique s'il s'agit d'un matricuel correctement formaté ou non
+     * @param inputToCheck La chaîne de caractère à vérifier
+     * @return VRAI s'il s'agit d'un matricule, FAUX sinon
+     */
+    private boolean checkRegistrationNumber(String inputToCheck) {
+
+        // Vérification de la taille de la chaîne de caractères
+        if (inputToCheck.length() != 7) {
+            return false;
+        }
+
+        // déclaration du booléen qui va stocker le résultat de la vérification
+        // VRAI -> la chaîne de caractères passée en paramètre est un matricule correctement formaté
+        // FAUX -> la chaîne de caractères passée en paramètre est un matricule pas bien formaté
+        boolean isRegistrationNumber = false;
+
+        // Cette première boucle permet de passer en revue TOUS les caractères de la chaîne
+        for (int index = 0; index < inputToCheck.length(); index++) {
+            // index = longueur de intupToCheck - 1 = 6
+            char ch = inputToCheck.charAt(index);
+            if (index == 0 || index == 1 || index == 5 || index == 6) {
+
+                // vérification du caractère, s'agit-il d'un chiffre ?
+                if (Character.isDigit(ch) == true) {
+                    isRegistrationNumber = true;
+                } else { // attention, ce n'est pas un chiffre
+                    return false;
+                }
+
+            } else { // cas de la position 2, 3 ou 4 -> vérification de letter
+
+                // Quelle opération dois-je faire ?
+                if (Character.isLetter(ch)) {
+                    isRegistrationNumber = true;
+                } else {
+                    return false;
+                }
+            }
+        } // fin du FOR, BRAVO !
+
+        return isRegistrationNumber;
+    }
+
+
+	// qui représente un objet de la classe employé
+	// plus d'information sur la méthode "toString()" ->
+	// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -1,21 +1,52 @@
 package fr.afpa.employees;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 /**
  * Classe principale du projet, contient la fonction "main"
  */
-class EmployeeMain
-{
-	public static void main(String[] args) 
-	{
+class EmployeeMain {
+	public static void main(String[] args) {
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
-		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
+		// Instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
+// Employee "c'est les types" employee1 'c'est le nom donnee au type" = "operateur de fonction" new "opérateur d'instanciation" Employee "nom du constructeur"
+		Employee employee1 = new Employee("11ABC22", "Dora", "Lexploratrice", 2500, "1920-06-22");
+
+		System.out.println(employee1.getRegistrationNumber());
+		 // Tentative de modification avec un matricule correctement formaté
+		 employee1.setRegistrationNumber("64AZE88");
+
+		 // test des matricules en erreur
+		 employee1.setRegistrationNumber("EEAZE88");
+ 
+		 employee1.setRegistrationNumber("6444433");
+ 
+		 employee1.setRegistrationNumber("64AZ");
+ 
+		 employee1.setRegistrationNumber("6");
+ 
+		 employee1.setRegistrationNumber("");
+		 
+		 employee1.setRegistrationNumber("987654");
 
 		// TODO afficher les informations des employés avec System.out.println
+		System.out.println(employee1.toString());
+
 	}
+
 }
 
+// Employee employee2 = new Employee("12ABC33", "Super", "Man", 8000,
+// 1980-12-31);
+// Employee employee3 = new Employee("13ABC44", "Michel", "Michel", 1200),
+// 2000-01-25;
+// Employee employee4 = new Employee("14ABC55", "Crocodile", "Dundes", 2500,
+// 1987-07-25);
+// System.out.println(employee2.toString());
+// System.out.println(employee3.toString());
+// System.out.println(employee4.toString());
