ad6b3baf3f445eb988e4fb359a9a2c33e9d90b8f
Amazir Lasri <lamazir@gmail.com>
2024-06-27 09:29:18 +0200

    Rajout des instances et implementations

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -20,86 +20,147 @@ public class Employee {
 	/**
 	 * Matricule de l'employé
 	 */
-	// compléter les attributs comme présenté dans le PDF
+	// compléter les attributs de classe comme présenté dans le PDF
 	private String registrationNumber;
 	private String lastName;
 	private String firstName;
 	private double salary;
-	private final int socialRate = 30;
+	private final int socialRate = 30; // Ne va pas bouger, non modifiable
 	private LocalDate birthday;
 
 	// compléter le constructeur de la classes
-	public Employee(String registrationNumber, String lastName, String firstName, double salary,
-			String birthday) {
+	// Public est un modificateur d'accees qui déterminent la visibilité et
+	// l'accessibilité des classes, méthodes, et variables depuis d'autres parties
+	// du code
+	// String retourne a une chaine de caracteres
+	// Corps du constructeur où les attributs seront initialisés ce qu'on appelle
+	// initialisation des attributs
+	// Le constructeur Employee initialise un nouvel objet Employee avec les valeurs
+	// fournies
+	// Il utilise les paramètres pour assigner des valeurs aux attributs
+	// correspondants de l'objet
+	// avec une conversion de la chaîne de caractères de la date de naissance en
+	// LocalDate.
+	public Employee(String registrationNumber, String lastName, String firstName, double salary, String birthday) {
 		this.registrationNumber = registrationNumber;
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.salary = salary;
-		this.birthday = LocalDate.parse(birthday);
+		this.birthday = LocalDate.parse(birthday); // Convertir la chaîne en LocalDate
 
 	}
 
-	// ajouter les getters et les setters
+	// implementer les getters et les setters de la classe
 
+	// l'implementation nous donne les donnees des getters
+	// Public est un modificateur d'accees qui déterminent la visibilité et
+	// l'accessibilité des classes, méthodes, et variables depuis d'autres parties
+	// du code
+	// String parametre de type String qui est une chaine de caracteres
+	// getRegistationNumber est le nom de la methode donne au getters
+	// elle retourne a une instance de class = objet
+	
 	// Getters
 	public String getRegistrationNumber() {
-		return registrationNumber;
+		return this.registrationNumber;
 	}
 
 	public String getLastName() {
-		return lastName;
+		return this.lastName;
 	}
 
 	public String getFirstName() {
-		return firstName;
+		return this.firstName;
 	}
 
 	public double getSalary() {
-		return salary;
+		return this.salary;
 	}
 
 	public int getSocialRate() {
-		return socialRate;
+		return this.socialRate;
 	}
 
 	public LocalDate getBirthday() {
-		return birthday;
+		return this.birthday;
 	}
 
+	// public : La méthode est accessible depuis n'importe quelle autre classe
+	// void : La méthode ne retourne aucune valeur
+	// setRegistrationNumber : Le nom de la méthode, utilisé pour définir ou
+	// modifier le matricule (registration number) de l'objet
+	// String registrationNumber : Paramètre d'entrée de la méthode, de type String
+	// (chaine de caractere) qui représente le nouveau matricule
+	// throws Exception : Indique que cette méthode peut lancer une exception de type Exception
+	// Vérification : La méthode checkRegistrationNumber est appelée pour vérifier si le nouveau matricule est valide
+	// checkRegistrationNumber(registrationNumber) : C'est une méthode (non montrée ici) qui retourne un booléen (true ou false) en fonction de la validité du registrationNumber
+	// Si valide (true) : La condition if (checkRegistrationNumber(registrationNumber) == true) est remplie 
+	// this.registrationNumber = registrationNumber; : L'attribut de l'objet registrationNumber est mis à jour avec la nouvelle valeur.
+	// Si invalide (false) : La condition else est exécutée.
+	// throw new Exception("ERREUR: Le nouveau registrationNumber n'est pas valide
+	// !"); : Une exception est levée, signalant que le nouveau matricule n'est pas
+	// valide.
+	
 	// Setters
-	public void setRegistrationNumber(String registrationNumber) {
-		this.registrationNumber = registrationNumber;
+	public void setRegistrationNumber(String registrationNumber) throws Exception {
+		// A ce niveau, tentative de modification du matricule
+		// Nous devons vérifier le matricule !
+		// ------> PROGRAMMING DEFENSIVE = vérification des paramètres d'entrée ->
+		// registrationNumber
+		if (checkRegistrationNumber(registrationNumber) == true) {
+			// VRAI donc matricule correct = Modification matricule
+			this.registrationNumber = registrationNumber;
+		} else {
+			// FAUX donc on fait qqch pour traiter ce cas
+			// EXCEPTION = ERREUR
+			throw new Exception("ERREUR: Le nouveau registrationNumber n'est pas valide !");
+		}
+
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setLastName(String lastName) throws Exception {
+		if (checkName(lastName) == true) {
+			this.lastName = lastName;
+		} else {
+			throw new Exception("ERREUR: Le lastName n'est pas valide !");
+		}
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setFirstName(String firstName) throws Exception {
+		if (checkName(firstName) == true) {
+			this.firstName = firstName;
+		} else {
+			throw new Exception("ERREUR: Le firstName n'est pas valide !");
+		}
 	}
 
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
 
-	public void setBirthday(LocalDate birthday) {
-		this.birthday = birthday;
-
-		// Implémenter les setters et getters de la classe (permet d'accéder aux
-		// attributs privés)
+	public void setBirthday(String birthday) throws Exception {
+		if (checkBirthDate(birthday) == true) {
+			this.birthday = LocalDate.parse(birthday);
+		} else {
+			throw new Exception("ERREUR: Le birthDay n'est pas valide !");
+		}
 
 	}
+	// Implémenter les setters et getters de la classe (permet d'accéder aux
+	// attributs public)
 
 	// Implémenter la méthode "toString()" qui renvoie une chaîne de caractère
 	@Override
 	public String toString() {
 		return "Employee [\n registrationNumber=" + registrationNumber
-				+ "\n lastName=" + lastName
-				+ "\n firstName=" + firstName
-				+ "\n salary=" + salary
-				+ "\n socialRate=" + socialRate
-				+ "\n birthday=" + birthday + "]";
+				+ "\n lastName = " + getLastName()
+				+ "\n firstName = " + getFirstName()
+				+ "\n salary = " + getSalary()
+				+ "\n netSalary = " + netSalary()
+				+ "\n socialRate = " + socialRate
+				+ "\n birthday = " + getBirthday()
+				+ "\n nextBirthday = " + daysBeforeBirthday()
+				+ "]";
 	}
 
 	public String fullName() {
@@ -118,53 +179,125 @@ public class Employee {
 		if (nextBirthday.isBefore(currentDate) || nextBirthday.isEqual(currentDate)) {
 			nextBirthday = nextBirthday.plusYears(1);
 		}
-		return ChronoUnit.DAYS.between(currentDate, currentDate);
+		return ChronoUnit.DAYS.between(currentDate, nextBirthday);
 	}
 
-/**
-     * Vérifie une chaîne de caractères et indique s'il s'agit d'un matricuel correctement formaté ou non
-     * @param inputToCheck La chaîne de caractère à vérifier
-     * @return VRAI s'il s'agit d'un matricule, FAUX sinon
-     */
-    private boolean checkRegistrationNumber(String inputToCheck) {
+	/**
+	 * Vérifie une chaîne de caractères et indique s'il s'agit d'un matricuel
+	 * correctement formaté ou non
+	 * 
+	 * @param inputToCheck La chaîne de caractère à vérifier
+	 * @return VRAI s'il s'agit d'un matricule, FAUX sinon
+	 */
+	private boolean checkRegistrationNumber(String inputToCheck) {
 
-        // Vérification de la taille de la chaîne de caractères
-        if (inputToCheck.length() != 7) {
-            return false;
-        }
+		// Vérification de la taille de la chaîne de caractères
+		if (inputToCheck.length() != 7) {
+			return false;
+		}
 
-        // déclaration du booléen qui va stocker le résultat de la vérification
-        // VRAI -> la chaîne de caractères passée en paramètre est un matricule correctement formaté
-        // FAUX -> la chaîne de caractères passée en paramètre est un matricule pas bien formaté
-        boolean isRegistrationNumber = false;
+		// déclaration du booléen qui va stocker le résultat de la vérification
+		// VRAI -> la chaîne de caractères passée en paramètre est un matricule
+		// correctement formaté
+		// FAUX -> la chaîne de caractères passée en paramètre est un matricule pas bien
+		// formaté
+		boolean isRegistrationNumber = false;
 
-        // Cette première boucle permet de passer en revue TOUS les caractères de la chaîne
-        for (int index = 0; index < inputToCheck.length(); index++) {
-            // index = longueur de intupToCheck - 1 = 6
-            char ch = inputToCheck.charAt(index);
-            if (index == 0 || index == 1 || index == 5 || index == 6) {
+		// Cette première boucle permet de passer en revue TOUS les caractères de la
+		// chaîne
+		for (int index = 0; index < inputToCheck.length(); index++) {
+			// index = longueur de intupToCheck - 1 = 6
+			char ch = inputToCheck.charAt(index);
+			if (index == 0 || index == 1 || index == 5 || index == 6) {
 
-                // vérification du caractère, s'agit-il d'un chiffre ?
-                if (Character.isDigit(ch) == true) {
-                    isRegistrationNumber = true;
-                } else { // attention, ce n'est pas un chiffre
-                    return false;
-                }
+				// vérification du caractère, s'agit-il d'un chiffre ?
+				if (Character.isDigit(ch) == true) {
+					isRegistrationNumber = true;
+				} else { // attention, ce n'est pas un chiffre
+					return false;
+				}
 
-            } else { // cas de la position 2, 3 ou 4 -> vérification de letter
+			} else { // cas de la position 2, 3 ou 4 -> vérification de letter
 
-                // Quelle opération dois-je faire ?
-                if (Character.isLetter(ch)) {
-                    isRegistrationNumber = true;
-                } else {
-                    return false;
-                }
-            }
-        } // fin du FOR, BRAVO !
+				// Quelle opération dois-je faire ?
+				if (Character.isLetter(ch)) {
+					isRegistrationNumber = true;
+				} else {
+					return false;
+				}
+			}
+		} // fin du FOR, BRAVO !
 
-        return isRegistrationNumber;
-    }
+		return isRegistrationNumber;
+	}
 
+	/**
+	 * Vérifie qu'une chaine de caractères passée en paramètre est un prénom ou un
+	 * nom
+	 * Pas de chiffres + caractères spé sauf '-'
+	 * 
+	 * @param inputToCheck La chaîne de caractères à vérifier
+	 * @return VRAI s'il s'agit d'un prénom/nom correctement formaté, FAUX sinon
+	 */
+
+	private boolean checkName(String inputToCheck) {
+		// déclaration du booléen qui va stocker le résultat de la vérification
+		// VRAI -> la chaîne de caractères passée en paramètre est un matricule
+		// correctement formaté
+		// FAUX -> la chaîne de caractères passée en paramètre est un matricule pas bien
+		// formaté
+
+		boolean isName = false;
+		for (int index = 0; index < inputToCheck.length(); index++) {
+			char ch = inputToCheck.charAt(index);
+			if (Character.isLetter(ch) == true || ch == '-') { // rappel || -> ou
+				isName = true;
+			} else {
+				return false;
+			}
+		}
+		return isName;
+	}
+
+	/**
+	 * Vérifie qu'une chaine de caractères passée en paramètre est une date de
+	 * naissance
+	 *
+	 * @param inputToCheck La chaîne de caractères à vérifier
+	 * @return VRAI s'il s'agit d'une date de naissance correctement formaté, FAUX
+	 *         sinon
+	 */
+	private boolean checkBirthDate(String inputToCheck) {
+
+		if (inputToCheck.length() != 10) {
+			return false;
+		}
+
+		boolean isBirthDate = false;
+		for (int index = 0; index < inputToCheck.length(); index++) {
+			// index = longueur de intupToCheck - 1 = 9
+			char ch = inputToCheck.charAt(index);
+			if (index == 0 || index == 1 || index == 2 || index == 3 || index == 5 || index == 6 || index == 8
+					|| index == 9) {
+
+				// vérification du caractère, s'agit-il d'un chiffre ?
+				if (Character.isDigit(ch) == true) {
+					isBirthDate = true;
+				} else { // attention, ce n'est pas un chiffre
+					return false;
+				}
+
+			} else { // cas de la position 4 et 7 -> vérification -
+
+				if (ch == '-') {
+					isBirthDate = true;
+				} else {
+					return false;
+				}
+			}
+		}
+		return isBirthDate;
+	}
 
 	// qui représente un objet de la classe employé
 	// plus d'information sur la méthode "toString()" ->
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -5,6 +5,7 @@ import java.time.Month;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
 /**
  * Classe principale du projet, contient la fonction "main"
@@ -14,39 +15,37 @@ class EmployeeMain {
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
 		// Instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-// Employee "c'est les types" employee1 'c'est le nom donnee au type" = "operateur de fonction" new "opérateur d'instanciation" Employee "nom du constructeur"
-		Employee employee1 = new Employee("11ABC22", "Dora", "Lexploratrice", 2500, "1920-06-22");
+		// Employee "c'est les types" employee1 'c'est le nom donnee au type" =
+		// "operateur de fonction" new "opérateur d'instanciation" Employee "nom du
+		// constructeur"
+		try {
+			Employee employee1 = new Employee("11ABC22", "Peppa", "Lavoyageuse", 2500, "1920-06-22");
+			System.out.println(employee1.getRegistrationNumber());
+			// Tentative de modification avec un matricule correctement formaté
+			employee1.setRegistrationNumber("64AZE88");
+			employee1.setLastName("Dora");
+			employee1.setFirstName("Lexploratrice");
 
-		System.out.println(employee1.getRegistrationNumber());
-		 // Tentative de modification avec un matricule correctement formaté
-		 employee1.setRegistrationNumber("64AZE88");
+			Employee employee2 = new Employee("11ABC22", "Peppa", "Lavoyageuse", 2500, "1920-06-22");
+			Employee employee3 = new Employee("12ABC33", "Super", "Man", 8000, "1980-12-31");
+			Employee employee4 = new Employee("13ABC44", "Michel", "Michel", 1200, "2000-01-25");
+			Employee employee5 = new Employee("14ABC55", "Crocodile", "Dundes", 2500, "1987-07-25");
 
-		 // test des matricules en erreur
-		 employee1.setRegistrationNumber("EEAZE88");
- 
-		 employee1.setRegistrationNumber("6444433");
- 
-		 employee1.setRegistrationNumber("64AZ");
- 
-		 employee1.setRegistrationNumber("6");
- 
-		 employee1.setRegistrationNumber("");
-		 
-		 employee1.setRegistrationNumber("987654");
+			// afficher les informations des employés avec System.out.println
+			ArrayList<Employee> listEmployees = new ArrayList<Employee>();
+			listEmployees.add(employee1);
+			listEmployees.add(employee2);
+			listEmployees.add(employee3);
+			listEmployees.add(employee4);
+			listEmployees.add(employee5);
 
-		// TODO afficher les informations des employés avec System.out.println
-		System.out.println(employee1.toString());
+			for (Employee employee : listEmployees) {
+				System.out.println(employee);
+			}
+
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		}
 
 	}
-
 }
-
-// Employee employee2 = new Employee("12ABC33", "Super", "Man", 8000,
-// 1980-12-31);
-// Employee employee3 = new Employee("13ABC44", "Michel", "Michel", 1200),
-// 2000-01-25;
-// Employee employee4 = new Employee("14ABC55", "Crocodile", "Dundes", 2500,
-// 1987-07-25);
-// System.out.println(employee2.toString());
-// System.out.println(employee3.toString());
-// System.out.println(employee4.toString());
